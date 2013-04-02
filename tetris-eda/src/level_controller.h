a1ccab6dd1ab3a317488b4536cc4220fcd79f46f
Elvis Stansvik <elvstone@gmail.com>
2013-04-02 15:49:43 +0200

    Change name to changeDifficultyLevel.

--- tetris-eda/src/level_controller.h
+++ tetris-eda/src/level_controller.h
@@ -8,14 +8,14 @@
 /**
  * Base class for difficulty level controllers.
  *
- * Level controllers inherit this class and emit the changeLevel signal
- * when they wish the change the difficulty level of the game. The
- * Board class is configured with a specific level controller, and
- * will react to the changeLevel signal.
+ * Level controllers inherit this class and emit the changeDifficultyLevel
+ * signal when they wish the change the difficulty level of the game. The
+ * Board class is configured with a specific level controller, and will
+ * react to the changeDifficultyLevel signal.
  *
  * At the moment there are two subclasses, LevelControllerEDA and
- * LevelControllerPerformance, each implementing their own algorithm
- * for changing the difficulty level.
+ * LevelControllerPerformance, each implementing their own algorithm for
+ * changing the difficulty level.
  */
 class LevelController : public QObject
 {
@@ -33,7 +33,7 @@ signals:
 	 *
 	 * @param level The requested difficulty level.
 	 */
-	void changeLevel(Board::DifficultyLevel level);
+	void changeDifficultyLevel(Board::DifficultyLevel level);
 };
 
 #endif // LEVEL_CONTROLLER_H
